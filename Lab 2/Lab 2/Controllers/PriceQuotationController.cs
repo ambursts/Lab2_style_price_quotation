1b73a49b6fc2c30a92c922bf52cd930dbc6f7065
ambursts <amboyd1005@gmail.com>
2026-05-02 00:47:09 -0400

    currently no errors

---
+++ Lab 2/Lab 2/Controllers/PriceQuotationController.cs
@@ -0,0 +1,21 @@
+﻿using Lab_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab_2.Controllers
+{
+    public class PriceQuotationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new PriceQuotationModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            return View(model);
+        }
+    }
+}
--- Lab 2/Lab 2/Models/PriceQuotationModel.cs
+++ Lab 2/Lab 2/Models/PriceQuotationModel.cs
@@ -1,6 +1,37 @@
-﻿namespace Lab_2.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Lab_2.Models
 {
     public class PriceQuotationModel
     {
-    }
+        [Required(ErrorMessage = "Please enter a subtotal.")]
+        [Range(0.01, double.MaxValue,
+                   ErrorMessage = "Subtotal must be greater than 0.")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(0, 100,
+            ErrorMessage = "Discount percent must be between 0 and 100.")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal GetDiscountAmount()
+        {
+            if (Subtotal.HasValue && DiscountPercent.HasValue)
+            {
+                return Subtotal.Value * DiscountPercent.Value / 100;
+            }
+            return 0;
+        }
+        public decimal GetTotal()
+        {
+            if (Subtotal.HasValue)
+            {
+                return Subtotal.Value - GetDiscountAmount();
+            }
+            return 0;
+        }
+
+
+
+
+        }
 }
